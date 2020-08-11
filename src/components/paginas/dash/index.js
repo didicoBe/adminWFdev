e402a2204898f57ea6a551d5e7835d554c4276e0
Waldir Desenvolvedor Web <wbn_k@hotmail.com>
2020-08-11 15:28:21 -0300

    add charts

--- src/components/paginas/dash/index.js
+++ src/components/paginas/dash/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Container, Card  } from 'react-bootstrap';
+import {Container, Card, Row, Col  } from 'react-bootstrap';
 import Topo from "../../topo";
 import SideBar from "../../sidebar";
 import  api from "../../../service";
@@ -91,8 +91,66 @@ export default class Dash extends Component {
                     />
                     <SideBar/>
                     <Container fluid>
-                        <Card className="cartaobase">
-
+                        <Card className="cartaoorcamento">
+                            <h5>Dashboard</h5>
+                            <Row>
+                                <Col md={4}>
+                                    <Card style={{ width: '18rem', marginTop:20 }}>
+                                        <Card.Body>
+                                            <Card.Title>Total deClientes</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of
+                                                the card's content.
+                                            </Card.Text>
+                                            <Card.Link href="#">Card Link</Card.Link>
+                                            <Card.Link href="#">Another Link</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={4}>
+                                    <Card style={{ width: '18rem', marginTop:20 }}>
+                                        <Card.Body>
+                                            <Card.Title>Total de Projetos</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of
+                                                the card's content.
+                                            </Card.Text>
+                                            <Card.Link href="#">Card Link</Card.Link>
+                                            <Card.Link href="#">Another Link</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={4}>
+                                    <Card style={{ width: '18rem', marginTop:20 }}>
+                                        <Card.Body>
+                                            <Card.Title>Total de ORçamentos</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of
+                                                the card's content.
+                                            </Card.Text>
+                                            <Card.Link href="#">Card Link</Card.Link>
+                                            <Card.Link href="#">Another Link</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={4}>
+                                    <Card style={{ width: '18rem', marginTop:20 }}>
+                                        <Card.Body>
+                                            <Card.Title>Faturamento Mensal</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of
+                                                the card's content.
+                                            </Card.Text>
+                                            <Card.Link href="#">Card Link</Card.Link>
+                                            <Card.Link href="#">Another Link</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
                         </Card>
                     </Container>
                 </div>
